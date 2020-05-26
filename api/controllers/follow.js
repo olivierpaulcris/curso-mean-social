186317dcab82b45c85729b53a0b416c1f6c662a8
Paul Cristerna <olivierpaulcristerna@gmail.com>
2020-05-26 15:35:33 -0600

    Se agregan nuevos cambios a las funciones de seguir usuarios.

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -54,7 +54,7 @@ function getFollowingUsers(req, res) {
       var userId = req.user.sub;
       var followId = req.params.id;
 
-      if (req.params.id) {
+      if (req.params.id && req.params.page) {
             userId = req.params.id;
       }
 
@@ -62,6 +62,8 @@ function getFollowingUsers(req, res) {
 
       if (req.params.page) {
             page = req.params.page;
+      } else {
+            page = req.params.id;
       }
 
       var itemsPerPage = 4;
@@ -81,9 +83,60 @@ function getFollowingUsers(req, res) {
       })
 };
 
+function getFollowedUsers(req, res) {
+      var userId = req.user.sub;
+
+      if (req.params.id && req.params.page) {
+            userId = req.params.id;
+      }
+
+      var page = 1;
+
+      if (req.params.page) {
+            page = req.params.page;
+      } else {
+            page = req.params.id;
+      }
+
+      var itemsPerPage = 4;
+
+      Follow.find({ followed: userId }).populate('user').paginate(page, itemsPerPage, (err, follows, total) => {
+            if (err) return res.status(500).send({
+                  message: "Error del servidor"
+            });
+
+            if (!follows) return res.status(404).send({ message: "No te sigue ningun usuario" });
+
+            return res.status(200).send({
+                  total: total,
+                  pages: Math.ceil(total / itemsPerPage),
+                  follows
+            });
+      })
+};
+
+function getMyFollows(req, res) {
+      var userId = req.user.sub;
+
+      Follow.find({ user: userId }).populate('user followed').exec((err, follows) => {
+            if (err) return res.status(500).send({
+                  message: "Error del servidor"
+            });
+
+            if (!follows) return res.status(404).send({ message: "No sigues a ningún usuario" });
+
+            return res.status(200).send({ follows });
+      });
+}
+
+function getFollowBacks(req, res) {
+
+}
+
 module.exports = {
       prueba,
       saveFollow,
       deleteFollow,
-      getFollowingUsers
+      getFollowingUsers,
+      getFollowedUsers
 }
--- api/routes/follow.js
+++ api/routes/follow.js
@@ -10,5 +10,6 @@ api.get('/pruebas-follow', md_auth.ensureAuth, FollowController.prueba);
 api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
 api.delete('/follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
 api.get('/following/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowingUsers);
+api.get('/followed/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowedUsers);
 
 module.exports = api;
